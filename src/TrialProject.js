eae086aea75fc8ff5152784015f8b0da582355bd
Phanord Roberto <phanordpicsouroberto11@gmail.com>
2021-12-23 15:13:55 -0400

    done

--- src/TrialProject.js
+++ src/TrialProject.js
@@ -11,7 +11,7 @@ function TrialProject() {
                 <h1 class="relative z-10 text-5xl font-bold leading-tight text-white lg:text-6xl pt-4">
                     Deep market insights at your fingertips
                 </h1>
-                <p class="py-4 text-xl tracking-wide leading-relaxed text-gray-300">Praesent tristique magma sit amet purus gravida quis blandit turpis.<br /> Enim nunc faucibus a pellentesque sit amet portitor eget. Enim lobortis scelerisque fermentum dui.</p>
+                <p class="py-4 text-xl tracking-wide leading-relaxed text-gray-300">Praesent tristique magma sit amet purus gravida quis blandit turpis. Enim nunc faucibus a pellentesque sit amet portitor eget. Enim lobortis scelerisque fermentum dui.</p>
                 <div class="py-2 grid grid-cols-1 md:grid-cols-2 gap-4 items-center w-full">
                     <div class="flex gap-x-4 items-center">
                         <img class="w-4 h-4" src={icon} alt="" />
